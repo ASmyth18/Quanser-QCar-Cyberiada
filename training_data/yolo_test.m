d7e0f5903b8d877e10af2e6671d4d93c31b05628
ASmyth18 <95776370+ASmyth18@users.noreply.github.com>
2026-04-01 05:06:35 +0100

    Delete training_data directory

--- training_data/yolo_test.m
+++
@@ -1,9 +0,0 @@
-detector = yolov4ObjectDetector('tiny-yolov4-coco');
-
-% Test on a frame that has signs/lights
-img = imread('C:\Users\adams\Desktop\Qunaser_Main_Project\training_data\frame\frame_00350.png');
-[bboxes, scores, labels] = detect(detector, img, 'Threshold', 0.3);
-
-% Show results
-detectedImg = insertObjectAnnotation(img, 'rectangle', bboxes, cellstr(labels));
-imshow(detectedImg);
